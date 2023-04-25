f04e46bf49ba6e2750bea0872de9eef624030040
rubenszinho <rubenszinho@users.noreply.github.com>
2023-04-25 14:53:33 +0000

    deploy: d43019f5365d502698e223a904e35c23876761a6

--- index.js
+++
@@ -1,9 +0,0 @@
-import init, { navigate_to_page } from "./pkg/navigation.js";
-
-async function run() {
-  await init();
-  navigate_to_page('home')
-  window.navigate_to_page = navigate_to_page;
-}
-
-run();
